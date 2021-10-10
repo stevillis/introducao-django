85b5ca92b1bea680a1899ee8ac39bb35da1d4ae2
Stévillis Sousa <stevillis@hotmail.com>
2021-10-10 16:52:49 -0400

    Change project name

--- introducao_django/wsgi.py
+++ introducao_django/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for escola project.
+WSGI config for introducao_django project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'escola.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'introducao_django.settings')
 
 application = get_wsgi_application()
